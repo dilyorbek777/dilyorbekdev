ba93a3615591f515fdf35281d4388b9519d30056
devdilyorbek <dilyorbekdev@gmail.com>
2024-06-04 14:46:50 +0500

    full pro all

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/dps.jpg" sizes="any" />
+        <link rel="icon" href="/dev/dps.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
